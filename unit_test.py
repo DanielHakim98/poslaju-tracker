5e49d312b4bd77b59db2de0a1eeb1c58a43bb543
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2023-06-05 08:20:37 +0800

    Fix unit testing

--- unit_test.py
+++ unit_test.py
@@ -4,16 +4,16 @@ import unittest
 
 class TestPL(unittest.TestCase):
     def test_valid_format(self):
-        valid_tn = "PL771883212383"
+        valid_tn = "PL77188321238"
         self.assertEqual(poslaju.formatPL(valid_tn), True, "Invalid tracking number")
 
-    def test_tn_not_14(self):
-        """Should be be exactly 14 characters long"""
-        not_14_char = "PL77188321238"
+    def test_tn_not_13(self):
+        """Should be be exactly 13 characters long"""
+        not_13_char = "PL7718832123"
         self.assertEqual(
-            poslaju.formatPL(not_14_char),
+            poslaju.formatPL(not_13_char),
             False,
-            "Should be be exactly 14 characters long",
+            "Should be be exactly 13 characters long",
         )
 
     def test_startwith_invalid_char(self):
