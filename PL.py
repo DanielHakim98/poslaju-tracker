59d3767bb019754b61c1ba41d6496275101d6faa
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2023-06-09 21:04:41 +0800

    Refractor and make it towards functional paradigm

--- PL.py
+++ PL.py
@@ -2,80 +2,97 @@ import requests
 import os
 
 
-# Clear Console On Windows/Linux
-def cls():
-    os.system("cls" if os.name == "nt" else "clear")
-
-
-# Format PosLaju Tracking Number To Be Accurate
-formatPL = lambda tr: len(tr) >= 13 and tr[0:2] in [
-    "EE",
-    "EH",
-    "EP",
-    "ER",
-    "EN",
-    "EM",
-    "PL",
-]
-
-
-def exit():
-    exiting = str(input("\nDo You Want To Continue? (Y/N):"))
-    exiting = exiting.upper()
-
-    print("\n")
-    if exiting == "Y":
-        cls()
-        main()
-    else:
-        return
-
-
 def main():
+    display_main_menu()
+    tr_num = generate_prompt("Tracking Number: ")
+    if is_tracking_number_valid(tr_num):
+        url = "https://ttu-svc.pos.com.my/api/trackandtrace/v1/request"
+        response_body = send_post_request(url, tr_num)
+        data = extract_data_value(response_body)
+        display_tracker_result(data)
+    else:
+        print(
+            "Wrong Tracking Number format. Please ensure the tracking number is correct."
+        )
+    is_exiting()
+
+
+def display_main_menu():
     print("#########################")
     print("# PosLaju Tracker v0.1 #")
     print("# Coded By Noor Aiman  #")
     print("########################\n")
 
-    tr_num = str(input("Tracking Number: "))
-    tr_num = tr_num.upper()
 
-    if formatPL(tr_num):
-        url = "https://ttu-svc.pos.com.my/api/trackandtrace/v1/request"
-        headers = {
-            "Accept": "application/json",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0",
-        }
-        body = {"connote_ids": [tr_num], "culture": "en"}
+def generate_prompt(message: str) -> str:
+    return input(message).upper()
 
-        try:
-            response = requests.post(url, headers=headers, json=body)
-            response_body = response.json()
-        except requests.RequestException as error_request:
-            print(
-                "There is an error while sending th request.\n Eror details: ",
-                error_request,
-            )
 
-        # Destructure response body (in json format) and only extract relevant information to users
-        data = response_body["data"][0]
-        print("\n======================================")
-        print("Your tracking number: ", data["connote_id"])
-        print("Process status: ", data["process_status"])
-        print("======================================")
-        is_available = lambda d, k: d.get(k, "N/A")
-        for idx, ele in enumerate(data["tracking_data"]):
-            print(f"\nTracking #{len(data['tracking_data']) -idx }")
-            print("--------------------------------")
-            print("date: ", is_available(ele, "date"))
-            print("process: ", is_available(ele, "process"))
-            print("process_summary: ", is_available(ele, "process_summary"))
-            print("office:", is_available(ele, "office"))
-    else:
-        print(
-            "Wrong Tracking Number format. Please ensure the tracking number is correct."
+def is_tracking_number_valid(tr: str) -> bool:
+    return len(tr) >= 13 and tr[0:2] in [
+        "EE",
+        "EH",
+        "EP",
+        "ER",
+        "EN",
+        "EM",
+        "PL",
+        "FD",
+    ]
+
+
+def send_post_request(url: str, tracking_number: str) -> dict:
+    try:
+        response = requests.post(
+            url, headers=get_http_headers(), json=get_http_payload(tracking_number)
         )
-    exit()
+        return response.json()
+    except requests.RequestException as error_request:
+        print(
+            "There is an error while sending th request.\n Eror details: ",
+            error_request,
+        )
+
+
+def get_http_headers() -> dict:
+    return {
+        "Accept": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0",
+    }
+
+
+def get_http_payload(tracking_number: str) -> dict:
+    return {"connote_ids": [tracking_number], "culture": "en"}
+
+
+def extract_data_value(response_body):
+    return response_body["data"][0]
+
+
+def display_tracker_result(data):
+    print("\n======================================")
+    print("Your tracking number: ", data["connote_id"])
+    print("Process status: ", data["process_status"])
+    print("======================================")
+    is_available = lambda d, k: d.get(k, "N/A")
+    for idx, ele in enumerate(data["tracking_data"]):
+        print(f"\nTracking #{len(data['tracking_data']) -idx }")
+        print("--------------------------------")
+        print("date: ", is_available(ele, "date"))
+        print("process: ", is_available(ele, "process"))
+        print("process_summary: ", is_available(ele, "process_summary"))
+        print("office:", is_available(ele, "office"))
+
+
+def is_exiting():
+    exit_answer = generate_prompt("\nDo You Want To Continue? (Y/N):")
+    if exit_answer == "Y":
+        clear_console()
+        main()
+
+
+def clear_console():
+    os.system("cls" if os.name == "nt" else "clear")
 
 
 if __name__ == "__main__":
--- unit_test.py
+++ unit_test.py
@@ -5,13 +5,15 @@ import unittest
 class TestPL(unittest.TestCase):
     def test_valid_format(self):
         valid_tn = "PL77188321238"
-        self.assertEqual(poslaju.formatPL(valid_tn), True, "Invalid tracking number")
+        self.assertEqual(
+            poslaju.is_tracking_number_valid(valid_tn), True, "Invalid tracking number"
+        )
 
     def test_tn_not_13(self):
         """Should be be more or equal 13 characters long"""
         not_13_char = "PL7718832123"
         self.assertEqual(
-            poslaju.formatPL(not_13_char),
+            poslaju.is_tracking_number_valid(not_13_char),
             False,
             "Should be be more or equal 13 characters long",
         )
@@ -23,7 +25,7 @@ class TestPL(unittest.TestCase):
         """
         invalid_startwith = "FF77188321238"
         self.assertEqual(
-            poslaju.formatPL(invalid_startwith),
+            poslaju.is_tracking_number_valid(invalid_startwith),
             False,
             """Should start with these code: ["EE", "EH", "EP", "ER", "EN", "EM", "PL"]""",
         )
