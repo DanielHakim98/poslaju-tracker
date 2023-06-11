9af43af303c05c01c4580c0388ca149552c14027
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2023-06-11 12:56:55 +0800

    Improve: Refractor displaying tracker result into several defs

--- PL.py
+++ PL.py
@@ -3,35 +3,37 @@ import os
 
 
 def main():
-    condition = True
-    while condition:
+    is_running_program = True
+    while is_running_program:
         clear_console()
         display_main_menu()
-        tr_num = generate_prompt("Tracking Number: ")
-        if is_tracking_number_valid(tr_num):
-            url = "https://ttu-svc.pos.com.my/api/trackandtrace/v1/request"
-            response_body = send_post_request(url, tr_num)
-            data = extract_data_value(response_body)
-            display_tracker_result(data)
-        else:
-            print(
-                "Wrong Tracking Number format. Please ensure the tracking number is correct."
-            )
-        condition = is_continuing()
+        start_user_interaction(generate_user_prompt("Tracking Number: "))
+        is_running_program = is_still_running_program()
 
 
-def display_main_menu():
+def clear_console() -> None:
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def display_main_menu() -> None:
     print("#########################")
     print("# PosLaju Tracker v0.1 #")
     print("# Coded By Noor Aiman  #")
     print("########################\n")
 
 
-def generate_prompt(message: str) -> str:
+def generate_user_prompt(message: str) -> str:
     return input(message).upper()
 
 
-def is_tracking_number_valid(tr: str) -> bool:
+def start_user_interaction(tr_num) -> None:
+    if is_valid_tracking_number_format(tr_num):
+        start_tracking(tr_num)
+    else:
+        print("Invalid Tracking Number Format.")
+
+
+def is_valid_tracking_number_format(tr: str) -> bool:
     return len(tr) >= 13 and tr[0:2] in [
         "EE",
         "EH",
@@ -42,6 +44,13 @@ def is_tracking_number_valid(tr: str) -> bool:
         "PL",
         "FD",
     ]
+
+
+def start_tracking(tr_num: str) -> None:
+    url = "https://ttu-svc.pos.com.my/api/trackandtrace/v1/request"
+    response_body = send_post_request(url, tr_num)
+    extracted_data = extract_data_from_response(response_body)
+    display_tracker_result(extracted_data)
 
 
 def send_post_request(url: str, tracking_number: str) -> dict:
@@ -68,32 +77,40 @@ def get_http_payload(tracking_number: str) -> dict:
     return {"connote_ids": [tracking_number], "culture": "en"}
 
 
-def extract_data_value(response_body: dict) -> dict:
+def extract_data_from_response(response_body: dict) -> dict:
     return response_body["data"][0]
 
 
-def display_tracker_result(data) -> None:
+def display_tracker_result(data: dict) -> None:
+    display_tracker_result_header(data)
+    for idx, ele in enumerate(data["tracking_data"]):
+        display_tracker_result_children(len(data["tracking_data"]) - idx, ele)
+
+
+def display_tracker_result_header(data: dict) -> None:
     print("\n======================================")
     print("Your tracking number: ", data["connote_id"])
-    print("Process status: ", data["process_status"])
+    print("Process status: ", return_value(data, "process_status"))
     print("======================================")
-    is_available = lambda d, k: d.get(k, "N/A")
-    for idx, ele in enumerate(data["tracking_data"]):
-        print(f"\nTracking #{len(data['tracking_data']) -idx }")
-        print("--------------------------------")
-        print("date: ", is_available(ele, "date"))
-        print("process: ", is_available(ele, "process"))
-        print("process_summary: ", is_available(ele, "process_summary"))
-        print("office:", is_available(ele, "office"))
 
 
-def is_continuing() -> bool:
-    exit_answer = generate_prompt("\nDo You Want To Continue? (Y/N):")
+def return_value(dict_data: dict, key: str) -> str:
+    value = dict_data.get(key, "N/A")
+    return value if len(value) > 0 else "N/A"
+
+
+def display_tracker_result_children(count: int, dict_data: dict) -> None:
+    print(f"\nTracking #{count}")
+    print("--------------------------------")
+    print("date: ", return_value(dict_data, "date"))
+    print("process: ", return_value(dict_data, "process"))
+    print("process_summary: ", return_value(dict_data, "process_summary"))
+    print("office:", return_value(dict_data, "office"))
+
+
+def is_still_running_program() -> bool:
+    exit_answer = generate_user_prompt("\nDo You Want To Continue? (Y/N):")
     return exit_answer == "Y"
-
-
-def clear_console() -> None:
-    os.system("cls" if os.name == "nt" else "clear")
 
 
 if __name__ == "__main__":
